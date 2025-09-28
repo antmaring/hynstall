8a9c5097004da439c5ba772987a87a3a4884f773
Ant Mar <antonio.maringonzalez@telefonica.com>
2025-09-28 07:36:03 +0200

    wall

--- dotfiles/hypr/script/wallpapers.sh
+++ dotfiles/hypr/script/wallpapers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 WALLPAPER_DIR="$HOME/.Images/Wallpapers/hypr"
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n1)
-swww img "$(echo "$WALLPAPER" | shuf -n1)" --outputs DP-3 --transition-duration 8 --transition-type wave --transition-angle 270
+swww img "$(echo "$WALLPAPER" | shuf -n1)" --transition-duration 8 --transition-type wave --transition-angle 270
